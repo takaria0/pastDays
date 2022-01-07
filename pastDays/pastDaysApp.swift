54a4f27a704fd2bb2af2f4808c85dc4e1c589661
gtaka0 <takajcb@gmail.com>
2022-01-07 17:38:14 +0900

    Initial Commit

---
+++ pastDays/pastDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  pastDaysApp.swift
+//  pastDays
+//
+//  Created by Takashi Inagaki on 2022/01/07.
+//
+
+import SwiftUI
+
+@main
+struct pastDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
